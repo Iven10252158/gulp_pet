7804017655df928d75eef60a134add9b08d2f6f8
IvenLiu <iven10252158@gmail.com>
2021-09-14 14:50:38 +0800

    第三次commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,15 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function(){  
   // Navbar按鈕
-  $('.bar').click(function(event){
+  $('span').click(function(event){
     event.preventDefault();
     $('.navBar-list').toggleClass('navBar-active');
   });
+  // 固定navbar
+  $(window).scroll(function(){
+    let scrollTop = $(window).scrollTop();
+    let navbarH = $('nav').height();
+    if ( scrollTop >= 1 ) {
+      $('nav').addClass('fixed')
+      $('body').css('paddingTop', navbarH)
+    } else {
+      $('nav').removeClass('fixed')
+      $('body').css('paddingTop', 0)
+    }
+  })
+  // 取消線上訂位的a預設事件
+  $('.online-reservation a').click(function(){
+    event.preventDefault();
+  });
+  // 往下滑的箭頭按鈕
+  $('.arrowBtn a').click(function(){
+    event.preventDefault();
+      $('html, body').animate({
+        scrollTop :1485
+      },100)
+  })
   // QA折疊選單
   $('.question li .answer ').click(function(e){
     e.preventDefault();
-    $(this).parent().find('span').toggleClass('active');
+    $(this).parent().find('a').toggleClass('active');
     $(this).parent().find('p').slideToggle();
     $(this).parent().siblings().find('p').slideUp();
-    $(this).parent().siblings().find('span').removeClass('active');
+    $(this).parent().siblings().find('a').removeClass('active');
   })
 });
